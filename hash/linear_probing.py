240a3f74cd2f385a8b84af7fb20b6a2fb55ac64d
Hojimatov <nh019849@m1515010.northamerica.cerner.net>
2017-04-15 16:58:14 -0500

    adding linear probing

---
+++ hash/linear_probing.py
@@ -0,0 +1,43 @@
+class Hash:
+    def __init__(self, size = 5):
+        self.size = size
+        self.table = [None for i in range(0, size)]
+
+    def get_hash_value(self, key, i):
+        hash_value = ((key % self.size) + i) % self.size
+        return hash_value
+
+    def insert(self, key):
+        i = 0
+        while i < self.size:
+            hash_value = self.get_hash_value(key, i)
+            if self.table[hash_value] == None:
+                self.table[hash_value] = key
+                return hash_value
+            else:
+                i = i + 1
+
+        raise Exception("Hash table is full")
+
+    def remove(self, key):
+        i = 0
+        while i < self.size:
+            hash_value = self.get_hash_value(key, i)
+            if self.table[hash_value] == key:
+                self.table[hash_value] = None
+                return hash_value
+            else:
+                i = i + 1
+
+        raise Exception("Key does not exist")
+
+    def does_key_exist(self, key):
+        i = 0
+        while i < self.size:
+            hash_value = self.get_hash_value(key, i)
+            if self.table[hash_value] == key:
+                return True
+            else:
+                i = i + 1
+
+        return False
