318f1041294ff636db30a0f453567661a89a07cc
Hojimatov <nh019849@m1515010.northamerica.cerner.net>
2017-04-21 21:59:29 -0500

    re-implementing binary search tree for practice

---
+++ practice/bst/bst.py
@@ -0,0 +1,90 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+        self.parent = None
+
+    def __str__(self):
+        return str(self.data)
+
+class BST:
+    def __init__(self):
+        self.root = None
+
+    def inorder_walk(self, node):
+        if node is not None:
+            self.inorder_walk(node.left)
+            print node.data
+            self.inorder_walk(node.right)
+
+    def parent(self, data):
+        parent = None
+        curr = self.root
+
+        while curr is not None:
+            parent = curr
+            if data < curr.data:
+                curr = curr.left
+            else:
+                curr = curr.right
+
+        return parent
+
+    def insert(self, data):
+        node = Node(data)
+        parent = self.parent(data)
+
+        if parent is None:
+            self.root = node
+        elif data < parent.data:
+            parent.left = node
+        else:
+            parent.right = node
+
+        node.parent = parent
+
+    def min(self, node):
+        min = None
+
+        while node is not None:
+            min = node
+            node = node.left
+
+        return min
+
+    def search(self, data):
+        curr = self.root
+        while curr is not None and data != curr.data:
+            if data < curr.data:
+                curr = curr.left
+            else:
+                curr = curr.right
+
+        return curr
+
+    def transplant(self, node, child):
+        if node.parent is None:
+            self.root = child
+        elif node.parent.left == node:
+            node.parent.left = child
+        else:
+            node.parent.right = child
+
+        if child is not None:
+            child.parent = node.parent
+
+    def remove(self, node):
+        if node.left is None:
+            self.transplant(node, node.right)
+        elif node.right is None:
+            self.transplant(node, node.left)
+        else:
+            min = self.min(node.right)
+            if min.parent != node:
+                self.transplant(min, min.right)
+                min.right = node.right
+                min.right.parent = min
+            self.transplant(node, min)
+            min.left = node.left
+            min.left.parent = min
---
+++ practice/bst/bst_tests.py
@@ -0,0 +1,34 @@
+import unittest
+from bst import BST
+
+class BSTTests(unittest.TestCase):
+    def setUp(self):
+        self.bst = BST()
+        self.bst.insert(10)
+        self.bst.insert(6)
+        self.bst.insert(15)
+        self.bst.insert(1)
+        self.bst.insert(20)
+
+    def test_inorder_walk(self):
+        self.bst.inorder_walk(self.bst.root)
+        self.assertEqual(0, 0)
+
+    def test_min(self):
+        min = self.bst.min(self.bst.root)
+        self.assertEqual(min.data, 1)
+
+    def test_search(self):
+        node = self.bst.search(20)
+        self.assertEqual(node.data, 20)
+
+    def test_remove(self):
+        node = self.bst.search(10)
+        self.bst.inorder_walk(self.bst.root)
+        self.bst.remove(node)
+        self.bst.inorder_walk(self.bst.root)
+        node = self.bst.search(10)
+        self.assertEqual(node, None)
+
+if __name__ == '__main__':
+    unittest.main()
