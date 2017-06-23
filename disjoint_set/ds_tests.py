2a0dfde6cda1b4603fa9b9a59dd7c506ecd58514
Hojimatov <nh019849@m1515010.northamerica.cerner.net>
2017-06-22 21:24:53 -0500

    implementing disjoint sets

--- disjoint_set/ds_tests.py
+++ disjoint_set/ds_tests.py
@@ -6,10 +6,10 @@ class DisjointSetTests(unittest.TestCase):
         self.ds = DisjointSet(['a', 'b', 'c', 'd', 'e', 'f'])
         self.ds.connected_components()
 
-    def test_contains(self):
+    def test_key(self):
         self.assertEqual(self.ds.sets[0].key, 'a')
 
-    def test_containss(self):
+    def test_find_set(self):
         parent = self.ds.find_set(self.ds.sets[5])
         self.assertEqual(parent.key, 'b')
 
